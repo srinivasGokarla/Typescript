1c9608c05f947125f3ad271ccadb77f8cab4b6f2
srinivasGokarla <srinivasgokarla@gmail.com>
2023-06-29 13:15:58 +0530

    typescript arrays practice

---
+++ arrays.ts
@@ -0,0 +1,47 @@
+let str = "srinivas"
+
+console.log(str)
+
+let names : string[] = ["sri", "nivas", "raju", "somu"];
+let names1 : Array<string> = ["sri", "nivas", "raju", "somu"];
+
+let items : (string | number | boolean)[] = ["fruits", 100, true, "cloths", 200 , false]
+for(let i= 0; i < items.length; i++) {
+    console.log(items[i])
+}
+
+let Items : Array<string | number | boolean> = ["fruits", 100, true, "cloths", 200 , false];
+for(let k in Items) {
+    console.log(Items[k])
+}
+console.log(names, names1, items, Items);
+
+let arr: number[] = [1,2,3,4,5,6,7,3,4];
+let array: Array<number> = [1,2,3,4,5,6,7,3,4];
+for(let i= 0; i < arr.length; i++){
+    console.log(arr[i])
+    }
+
+let multarray : number[][] = [[1,2], [3,4],[5,6]];
+for(let i= 0; i < multarray.length; i++) {
+    for(let j = 0; j < multarray[i].length; j++) {
+        console.log(multarray[i][j])
+    }
+}
+
+let multiarray1 : (string | number)[][] = [[1,"xyz"], [2,"bcd"]]
+for(let i in multiarray1) {
+    for(let j in multiarray1[i]) {
+        console.log(multiarray1[i][j])
+    }
+}
+
+
+
+
+ 
+
+
+
+
+
---
+++ practice.js
@@ -0,0 +1,47 @@
+// const ab=["a0","b1","c2","d3"] // [a3,b2,c1,d0]
+
+// const ans = a.map((item[i]) => item.split("")
+// )
+// console.log(ans)
+
+// const a=["a","b","c","d"] // [a3,b2,c1,d0]
+
+// //console.log(a)
+// let arr = []
+// let j = a.length
+// for(let i = 0; i< a.length; i++) {
+//     j--;
+//  arr.push(`${a[i]}${j}`)
+
+// }
+// console.log(arr)
+// const b=a.reverse().map((name,index)=>name+index)
+// console.log(b.reverse())
+
+// const c = ab.map((name, index) => name + index)
+// console.log(c.reverse())
+
+// const arr = [1,2,23,3,5,5,2,5,2,2,1,5]
+
+// console.log(new Set(arr))
+
+const string = "aabbbaxcdebcefg"
+
+let str = string.split("")
+let obj = {}
+let arr  = []
+for(let i= 0; i < str.length; i++) {
+    if(obj[str[i]] === undefined) {
+        obj[str[i]] = 1
+    } else {
+        obj[str[i]] = obj[str[i]] +1 
+    }
+}
+for( let k in obj) {
+   arr.push(k)
+}
+console.log(arr.join(""))
+let unique = str.filter((item, i, ar) => ar.indexOf(item) === i);
+let ans = new Set(str)
+console.log(ans)
+console.log(unique.join(""))
