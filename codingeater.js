3158255ef28a19fcde9cd1fd6d2dcabb0a9443d2
srinivasGokarla <Srinivas@dxminds.com>
2023-07-06 17:51:57 +0530

    dsa

--- codingeater.js
+++ codingeater.js
@@ -22,22 +22,22 @@ function addElementsIfGreater(inputArray) {
  // let outputArray = addElementsIfGreater(inputArray);
   
   console.log(outputArray);
-  // let sum = inputArray[0]
-  // let arr = []
-  // for(let i= 0; i < inputArray.length; i++) {
-  //     let current = inputArray[i]
-  //     if(current <= sum) {
-  //         sum = current + sum;
-  //         arr.push(sum)
-  //     } else {
-  //         console.log(arr)
-  //     }
-  // }
-  // console.log(arr)
-  //  let out = []
-  //  let init = 0
-  //  let ans = inputArray.reduce((item,accum) => accum + item, init
-  //  )
+   let sum = inputArray[0]
+  let arr = []
+  for(let i= 0; i < inputArray.length; i++) {
+      let current = inputArray[i]
+      if(current <= sum) {
+          sum = current + sum;
+          arr.push(sum)
+      } else {
+          console.log(arr)
+      }
+  }
+  console.log(arr)
+   let out = []
+   let init = 0
+   let ans = inputArray.reduce((item,accum) => accum + item, init
+   )
    
-   //console.log(ans)
+   console.log(ans)
   
